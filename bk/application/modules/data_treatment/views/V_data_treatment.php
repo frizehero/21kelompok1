d0e9dc8e982ac1eba970e8c6dd6e04c427ed4944
achmadmuhajir <77608335+achmadmuhajir@users.noreply.github.com>
2021-03-01 09:03:58 +0700

    Update V_data_treatment.php

--- bk/application/modules/data_treatment/views/V_data_treatment.php
+++ bk/application/modules/data_treatment/views/V_data_treatment.php
@@ -23,7 +23,9 @@
 
     <div class="text-right breadcrumb">
       <div id="demo-custom-toolbar5" class="table-toolbar-left">
-        <a class="btn btn-default text-left "   data-toggle="modal" data-target="#demo-default-tambah">Tambah</a>
+        <button class="btn btn-default text-left "   data-toggle="modal" data-target="#demo-default-tambah">
+          Tambah
+        </button>
       </div>
       <form action="<?php echo site_url('data_treatment/cari') ?>" method="post" class="col-xs-8 col-sm-3 text-right">
         <div class="input-group text-right"  style="padding-left: : 5px">
@@ -37,213 +39,226 @@
           <div class="input-group-btn  text-right"  style="padding-left: : 10px">
             <button class="btn btn-default" type="submit">cari</button>
           </div>
-        <a class="btn btn-success form-control"  style="padding-left: : 10px" href="<?php echo base_url('data_treatment'); ?>">
+          <a class="btn btn-success form-control"  style="padding-left: : 10px" href="<?php echo base_url('data_treatment'); ?>">
             <i class="fa fa-refresh" ></i>
           </a>
-      </div> 
-    </center>
+        </div> 
+      </center>
 
-  </form>
-</div>
-</div>
+    </form>
+  </div>
+</div><br><br><br>
 
 <div id="page-content">
-    <?php foreach($tampil as $res) {
+
+  <div class="row">
+
+    <div class="col-sm-12">
+      <div class="row">
+       <?php foreach($tampil as $res) {
         $id = $res->id_treatment;
-    ?>
-    <div class="col-lg-3" style="margin-top: 4%">
-        
-            <div class="panel panel-info panel-colorful widget" style="height:110px">
-                                                <!--Profile Widget-->
-                                                <!--===================================================-->
-                                                <div class="panel panel-info panel-colorful">
-                                                    <div class="pad-all text-left">
-                                                         <?= $res->nama_treatment ?>
-                                                         <span class="pull-right" style="float: right;"></span><?= $res->point ?><br><br><br>
-                                                        <div class="btn-group">
-                                                          <span>
-                                                            <a data-toggle="modal" data-target="#demo-default-modal1<?php echo $res->id_treatment?>" class=" btn btn-warning btn-sm">
-                                                            <span class="fa fa-edit"></span>&nbsp;Edit</a>
-                                                          </span>
-                                                          <span>
-                                                            <a data-toggle="modal" data-target="#demo-default-modal2<?php echo $res->id_treatment?>" class=" btn btn-danger btn-sm">
-                                                            <span class="fa fa-trash"></span>&nbsp;Hapus</a>
-                                                          </span>
-                                                      </div>
-                                                    </div>
-                                                </div>
-                          
-            </div>
+        ?>
+        <div class="col-sm-3">
 
-            <!-- update -->
-            <div class="modal fade" id="demo-default-modal1<?php echo $res->id_treatment?>" role="dialog" tabindex="-1" aria-labelledby="demo-default-modal" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
+          <!--Profile Widget-->
+          <!--===================================================-->
+          <div class="panel panel-info panel-colorful">
+            <div class="pad-all text-left">
+              <span class="pull-right"><?php echo $res->point ?></span><br>
+              <p><?php echo $res->nama_treatment ?></p>
 
-                <!--Modal Update-->
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal"><i class="pci-cross pci-circle"></i></button>
-                    <h4 class="modal-title">Update</h4>
-                </div>
-                  <?= form_open_multipart('data_treatment/edit'); ?>
-                <input type="hidden" name="id_treatment" value="<?php echo $res->id_treatment?>">
+              <div class="btn-group btn-group-justified pad-top">
 
-                <!--Modal body--> 
-                <div class="modal-body">
-                    
-                                    <div class="panel-body">
-                                        
-                                        <div class="col-md-6">
-                                            <label for="" class="control-label">Nama treatment</label>
-                                            <input type="text" name="nama_treatment" placeholder="Nama treatment" class="form-control" value="<?= $res->nama_treatment ?>">
-                                        </div>
-                                         
-                                         <div class="col-md-6" style="margin-top: 2%">
-                                            <label for="" class="control-label">point</label>
-                                            <textarea type="text" name="point" rows="5" placeholder="point" class="form-control"><?= $res->point ?></textarea>
-                                        </div>
-                                      
-                                        
-                                    </div>
-                                   
-                               
-                </div>
-
-                <!--Modal footer-->
-                <div class="modal-footer">
-                    <button data-dismiss="modal" class="btn btn-default" type="button">Close</button>
-                    <button class="btn btn-primary" type="submit">Simpan</button>
-                </div>
-                <?= form_close(); ?>
-            </div>
+               <span>
+                <a data-toggle="modal" data-target="#demo-default-modal1<?php echo $res->id_treatment?>" class=" btn btn-success btn-sm">
+                  <span class="fa fa-edit"></span>
+                  &nbsp;Edit
+                </a>
+              </span>
+              <span>
+               <a data-toggle="modal" data-target="#demo-default-modal2<?php echo $res->id_treatment?>" class=" btn btn-danger btn-sm">
+                <span class="fa fa-trash"></span>
+                &nbsp;Hapus
+              </a>
+            </span> 
+          </div>
         </div>
+      </div>
+      </div> <?php } ?> 
     </div>
-            <!-- end update -->
 
-    <!-- detail -->
-            
-  
-            <!-- end detail -->
+  </div>
+</div>
 
-    <!-- hapus -->
-    <div class="modal fade" id="demo-default-modal2<?php echo $res->id_treatment?>" role="dialog" tabindex="-1" aria-labelledby="demo-default-modal" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
+<!-- update -->
+<div class="modal fade" id="demo-default-modal1<?php echo $res->id_treatment?>" role="dialog" tabindex="-1" aria-labelledby="demo-default-modal" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
 
-                <!--Modal header-->
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal"><i class="pci-cross pci-circle"></i></button>
-                    <h4 class="modal-title">Hapus</h4>
-                </div>
+      <!--Modal Update-->
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><i class="pci-cross pci-circle"></i></button>
+        <h4 class="modal-title">Update</h4>
+      </div>
+      <?= form_open_multipart('data_treatment/edit'); ?>
+      <input type="hidden" name="id_treatment" value="<?php echo $res->id_treatment?>">
 
-                <!--Modal body-->
-                <div class="modal-body">
-                    <p class="text-semibold text-main"></p>
-                    <p>Anda Yakin Ingin Menghapus <b><?php echo $res->nama_treatment ?></b> ? </p>
-                    <br>
-                   
-                   
-                    
-                </div>
+      <!--Modal body--> 
+      <div class="modal-body">
 
-                <!--Modal footer-->
-                <div class="modal-footer">
-                    <button data-dismiss="modal" class="btn btn-default" type="button">Batal</button>
-                    <a class="btn btn-danger" href="<?php echo base_url('data_treatment/hapus/'. $res->id_treatment) ?>">Hapus treatment</a>
-                </div>
-            </div>
+        <div class="panel-body">
+
+          <div class="col-md-6">
+            <label for="" class="control-label">Nama treatment</label>
+            <input type="text" name="nama_treatment" placeholder="Nama treatment" class="form-control" value="<?= $res->nama_treatment ?>">
+          </div>
+
+          <div class="col-md-6">
+            <label for="" class="control-label">point</label>
+            <input type="text" name="point"  placeholder="point" class="form-control" value="<?= $res->point ?>">
+          </div>
+
+
         </div>
-    </div>
-    <!-- end hapus -->
-        
-    </div>
-    <?php } ?>
 
-    <!-- tambah -->
-        <div class="modal fade" id="demo-default-tambah" role="dialog" tabindex="-1" aria-labelledby="demo-default-modal" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
 
-                <!--Modal Update-->
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal"><i class="pci-cross pci-circle"></i></button>
-                    <h4 class="modal-title">Tambah</h4>
-                </div>
-                 
-                <?= form_open_multipart('data_treatment/tambah'); ?>
-                <!--Modal body--> 
-                <div class="modal-body">
-                    
-                                    <div class="panel-body">
-                                        
-                                        <div class="col-md-6">
-                                            <label for="" class="control-label">Nama treatment</label>
-                                            <input type="text" name="nama_treatment" placeholder="Nama treatment" class="form-control">
-                                        </div>
-                                         
-                                         <div class="col-md-6">
-                                            <label for="" class="control-label">point</label>
-                                            <input type="text" name="point" rows="5" placeholder="point" class="form-control">
-                                        </div>
-                                        
-                                        
-                                    </div>
-                                   
-                               
-                </div>
+      </div>
 
-                <!--Modal footer-->
-                <div class="modal-footer">
-                    <button data-dismiss="modal" class="btn btn-default" type="button">Close</button>
-                    <button class="btn btn-primary" type="submit">Simpan</button>
-                </div>
-                <?= form_close(); ?>
-                 
-            </div>
-        </div>
+      <!--Modal footer-->
+      <div class="modal-footer">
+        <button data-dismiss="modal" class="btn btn-default" type="button">Close</button>
+        <button class="btn btn-primary" type="submit">Simpan</button>
+      </div>
+      <?= form_close(); ?>
     </div>
-            <!-- end tambah -->
+  </div>
+</div>
+<!-- end update -->
+
+<!-- detail -->
+
+
+<!-- end detail -->
+
+<!-- hapus -->
+<div class="modal fade" id="demo-default-modal2<?php echo $res->id_treatment?>" role="dialog" tabindex="-1" aria-labelledby="demo-default-modal" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!--Modal header-->
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><i class="pci-cross pci-circle"></i></button>
+        <h4 class="modal-title">Hapus</h4>
+      </div>
+
+      <!--Modal body-->
+      <div class="modal-body">
+        <p class="text-semibold text-main"></p>
+        <p>Anda Yakin Ingin Menghapus <b><?php echo $res->nama_treatment ?></b> ? </p>
+        <br>
+
+
+
+      </div>
+
+      <!--Modal footer-->
+      <div class="modal-footer">
+        <button data-dismiss="modal" class="btn btn-default" type="button">Batal</button>
+        <a class="btn btn-danger" href="<?php echo base_url('data_treatment/hapus/'. $res->id_treatment) ?>">Hapus treatment</a>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- end hapus -->
 
 </div>
- <!--jQuery [ REQUIRED ]-->
-      <script src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
-
-      <script>
-          $(document).ready(function () {
 
 
-            $('#demo-dp-txtinputmasukkerja input').datepicker({
-              format: "yyyy-m-d",
-              todayBtn: "linked",
-              todayHighlight: true
-            });
+<!-- tambah -->
+<div class="modal fade" id="demo-default-tambah" role="dialog" tabindex="-1" aria-labelledby="demo-default-modal" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
 
-            $('#demo-dp-txtinputakhirkerja input').datepicker({
-              format: "yyyy-m-d",
-              todayBtn: "linked",
-              todayHighlight: true
-            });
+      <!--Modal Update-->
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><i class="pci-cross pci-circle"></i></button>
+        <h4 class="modal-title">Tambah</h4>
+      </div>
+
+      <?= form_open_multipart('data_treatment/tambah'); ?>
+      <!--Modal body--> 
+      <div class="modal-body">
+
+        <div class="panel-body">
+
+          <div class="col-md-6">
+            <label for="" class="control-label">Nama treatment</label>
+            <input type="text" name="nama_treatment" placeholder="Nama treatment" class="form-control">
+          </div>
+
+          <div class="col-md-6">
+            <label for="" class="control-label">point</label>
+            <input type="text" name="point" rows="5" placeholder="point" class="form-control">
+          </div>
+
+
+        </div>
+
+
+      </div>
+
+      <!--Modal footer-->
+      <div class="modal-footer">
+        <button data-dismiss="modal" class="btn btn-default" type="button">Close</button>
+        <button class="btn btn-primary" type="submit">Simpan</button>
+      </div>
+      <?= form_close(); ?>
+
+    </div>
+  </div>
+</div>
+<!-- end tambah -->
+
+</div>
+<!--jQuery [ REQUIRED ]-->
+<script src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
+
+<script>
+  $(document).ready(function () {
+
+
+    $('#demo-dp-txtinputmasukkerja input').datepicker({
+      format: "yyyy-m-d",
+      todayBtn: "linked",
+      todayHighlight: true
+    });
+
+    $('#demo-dp-txtinputakhirkerja input').datepicker({
+      format: "yyyy-m-d",
+      todayBtn: "linked",
+      todayHighlight: true
+    });
 
 
 
-          });
-      </script>
-      
+  });
+</script>
+
 
 <script type="text/javascript">
 
 
-function tampilkanPreview(userfile,idpreview)
-{
-  var gb = userfile.files;
-  for (var i = 0; i < gb.length; i++)
+  function tampilkanPreview(userfile,idpreview)
   {
-    var gbPreview = gb[i];
-    var imageType = /image.*/;
-    var preview=document.getElementById(idpreview);
-    var reader = new FileReader();
-    if (gbPreview.type.match(imageType))
+    var gb = userfile.files;
+    for (var i = 0; i < gb.length; i++)
     {
+      var gbPreview = gb[i];
+      var imageType = /image.*/;
+      var preview=document.getElementById(idpreview);
+      var reader = new FileReader();
+      if (gbPreview.type.match(imageType))
+      {
       //jika tipe data sesuai
       preview.file = gbPreview;
       reader.onload = (function(element)
@@ -256,24 +271,24 @@ function tampilkanPreview(userfile,idpreview)
       //membaca data URL gambar
       reader.readAsDataURL(gbPreview);
     }
-      else
-      {
+    else
+    {
         //jika tipe data tidak sesuai
         alert("Tipe file tidak sesuai. Gambar harus bertipe .png, .gif atau .jpg.");
       }
+    }
   }
-}
-function tampilkanPreview1(userfile,idpreview1)
-{
-  var gb = userfile.files;
-  for (var i = 0; i < gb.length; i++)
+  function tampilkanPreview1(userfile,idpreview1)
   {
-    var gbPreview1 = gb[i];
-    var imageType = /image.*/;
-    var preview1=document.getElementById(idpreview1);
-    var reader = new FileReader();
-    if (gbPreview1.type.match(imageType))
+    var gb = userfile.files;
+    for (var i = 0; i < gb.length; i++)
     {
+      var gbPreview1 = gb[i];
+      var imageType = /image.*/;
+      var preview1=document.getElementById(idpreview1);
+      var reader = new FileReader();
+      if (gbPreview1.type.match(imageType))
+      {
       //jika tipe data sesuai
       preview1.file = gbPreview1;
       reader.onload = (function(element)
@@ -286,11 +301,11 @@ function tampilkanPreview1(userfile,idpreview1)
       //membaca data URL gambar
       reader.readAsDataURL(gbPreview1);
     }
-      else
-      {
+    else
+    {
         //jika tipe data tidak sesuai
         alert("Tipe file tidak sesuai. Gambar harus bertipe .png, .gif atau .jpg.");
       }
+    }
   }
-}
 </script>
