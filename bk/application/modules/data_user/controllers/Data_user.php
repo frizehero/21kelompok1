e7f561e2b74b86cc2516cf04c7f31b376b5c4610
khakim <77608259+khakim123@users.noreply.github.com>
2021-03-03 13:34:12 +0700

    Davino : Perbaikan
    Perbaikan data user

--- bk/application/modules/data_user/controllers/Data_user.php
+++ bk/application/modules/data_user/controllers/Data_user.php
@@ -1,55 +1,55 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+ <?php
+ defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Data_user extends MX_Controller {
+ class Data_user extends MX_Controller {
 
-	function __construct()
-	{
-		parent::__construct();
+ 	function __construct()
+ 	{
+ 		parent::__construct();
 		// model
-		 $this->load->model('m_data_user');
-		 $this->load->model('login/m_session');
-	}
+ 		$this->load->model('m_data_user');
+ 		$this->load->model('login/m_session');
+ 	}
 
-	
+ 	
 	// index
-	function index()
-	{
-		$data = array(
-			'namamodule' 	=> "data_user",
-			'namafileview' 	=> "V_data_user",
-			'tampil'		=> $this->m_data_user->tampil(),
-		);
-		echo Modules::run('template/tampilCore', $data);
-	}
+ 	function index()
+ 	{
+ 		$data = array(
+ 			'namamodule' 	=> "data_user",
+ 			'namafileview' 	=> "V_data_user",
+ 			'tampil'		=> $this->m_data_user->tampil(),
+ 		);
+ 		echo Modules::run('template/tampilCore', $data);
+ 	}
 
-	function tambah()
-	{
-		$this->m_data_user->tambah();
-		redirect('data_user');
-	}
+ 	function tambah()
+ 	{
+ 		$this->m_data_user->tambah();
+ 		redirect('data_user');
+ 	}
 
-	function edit()
-	{
-		$this->m_data_user->edit();
-		redirect('data_user');
-	}
+ 	function edit()
+ 	{
+ 		$this->m_data_user->edit();
+ 		redirect('data_user');
+ 	}
 
-	function hapus($id)
-	{
-		$this->m_data_user->hapus($id);
-		redirect('data_user');
-	}
+ 	function hapus($id)
+ 	{
+ 		$this->m_data_user->hapus($id);
+ 		redirect('data_user');
+ 	}
 
-	function cari()
-	{
-		$data = array(
-			'namamodule' 	=> "data_user",
-			'namafileview' 	=> "V_data_user",
-			'tampil'		=> $this->m_data_user->cari(),
-		);
-		echo Modules::run('template/tampilCore', $data);
-	}
-	
-}
+ 	function cari()
+ 	{
+ 		$data = array(
+ 			'namamodule' 	=> "data_user",
+ 			'namafileview' 	=> "V_data_user",
+ 			'tampil'		=> $this->m_data_user->cari(),
+ 		);
+ 		echo Modules::run('template/tampilCore', $data);
+ 	}
+ 	
+ }
  
