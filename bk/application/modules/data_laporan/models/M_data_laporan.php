952d3aebe2ae9e8299d2a5d1b45f8ccbb747c920
khakim <77608259+khakim123@users.noreply.github.com>
2021-03-31 14:20:33 +0700

    perbaikan jumlah siswa

--- bk/application/modules/data_laporan/models/M_data_laporan.php
+++ bk/application/modules/data_laporan/models/M_data_laporan.php
@@ -11,4 +11,28 @@ class M_data_laporan extends CI_Model {
 	{
 	}
 
+	function jum_sis(){
+		$this->db->select('*')
+		->from('data_siswa');
+		$query = $this->db->get();
+		return $query->num_rows();
+	}
+
+	function jum_laki()
+	{
+		$this->db->select('*')
+		->like('jenis_kelamin_siswa','Laki-Laki');
+		$query = $this->db->get('data_siswa');
+		return $query->num_rows();
+	}
+
+
+	function jum_perempuan()
+	{
+		$this->db->select('*')
+		->like('jenis_kelamin_siswa','Perempuan');
+		$query = $this->db->get('data_siswa');
+		return $query->num_rows();
+	}
+
 }
