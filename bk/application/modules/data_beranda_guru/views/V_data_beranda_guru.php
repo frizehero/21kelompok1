10dceb0b06c04d5e635d5a8d30c88f9c3e6272fe
achmadmuhajir <77608335+achmadmuhajir@users.noreply.github.com>
2021-04-27 03:46:12 +0700

    Update V_data_beranda_guru.php

--- bk/application/modules/data_beranda_guru/views/V_data_beranda_guru.php
+++ bk/application/modules/data_beranda_guru/views/V_data_beranda_guru.php
@@ -1,13 +1,13 @@
 <div class="boxed">
 
-                <div id="page-head">
+    <div id="page-head">
 
-                    <div class="pad-all text-center">
-                        <h3>Selamat Datang Di Website BK</h3>
-                        <p1>SMK BISA SMK HEBAT</p1>
-                    </div>
+        <div class="pad-all text-center">
+            <h3>Selamat Datang Di Website BK</h3>
+            <p1>SMK BISA SMK HEBAT</p1>
+        </div>
 
-                </div>
+    </div>
 
 </div>
 
@@ -19,17 +19,17 @@
             <div class="panel panel-warning panel-colorful media middle pad-all">
                 <div class="media-left">
                     <div class="pad-hor ion-ios-bookmarks-outline icon-3x">
-                                            
+
                     </div>
                 </div>
                 <div class="media-body">
-                    <p class="text-2x mar-no text-semibold">1125</p>
+                    <p class="text-2x mar-no text-semibold"><?php echo $pelanggar_hariini ?></p>
                     <p class="mar-no">Pelanggaran Hari Ini</p>
                 </div>
             </div>
         </div>
 
-         <div class="col-md-3">
+        <div class="col-md-3">
             <div class="panel panel-info panel-colorful media middle pad-all">
                 <div class="media-left">
                     <div class="pad-hor">
@@ -50,11 +50,11 @@
             <div class="panel panel-mint panel-colorful media middle pad-all">
                 <div class="media-left">
                     <div class="pad-hor ti-agenda icon-3x">
-                                            
+
                     </div>
                 </div>
                 <div class="media-body">
-                    <p class="text-2x mar-no text-semibold">33</p>
+                    <p class="text-2x mar-no text-semibold"><?php echo $pterbaru ?></p>
                     <p class="mar-no">Total Pelanggaran</p>
                 </div>
             </div>
@@ -64,11 +64,11 @@
             <div class="panel panel-danger panel-colorful media middle pad-all">
                 <div class="media-left">
                     <div class="pad-hor ti-write icon-3x">
-                                            
+
                     </div>
                 </div>
                 <div class="media-body">
-                    <p class="text-2x mar-no text-semibold">9</p>
+                    <p class="text-2x mar-no text-semibold"><?php echo $tterbaru ?></p>
                     <p class="mar-no">Total Treatment</p>
                 </div>
             </div>
@@ -77,17 +77,27 @@
     </div>
 
     <div class="panel">
-        <form class="form-horizontal">
-            <div class="panel-body">
-                <div class="form-group">
-                    <label class="col-sm-4 control-label" for="demo-hor-inputemail">Masukkan Nis/Nama</label>
-                        <a class="btn btn-primary" type="submit" href="ui-modals.html">Cari</a>
-                    <div class="col-sm-5">
-                        <input type="Masukkan Nis/Nama" placeholder="Masukkan Nis/Nama" class="form-control">
-                    </div>
-                </div>                     
-            </div>              
-        </form>    
-    </div>
+        <div class="panel-body">
+          <form action="<?php echo site_url('data_beranda_guru/carisiswa/') ?>" method="post" class="col-sm-10 text-right">
+            <label class="col-sm-4 control-label" for="demo-hor-inputemail">Masukkan Nis/Nama</label>
+            <div class="input-group text-right"  style="padding-left: : 5px">
+              <?php if($this->uri->segment(2) != 'cari'){?>
+                <input type="text" autocomplete="off" name="cari" class="form-control" placeholder="Cari Nis/Nama" required="">
+            <?php }
+            ?>
+            <?php if($this->uri->segment(2) == 'cari'){
+                $cari = $this->input->post('cari'); ?>
+                <input type="text" autocomplete="off" value="<?= $cari ?>" name="cari" class="form-control " placeholder="Cari Nis/Nama" required="">
+            <?php } ?> 
+            <div class="input-group-btn  text-right"  style="padding-left: : 10px">
+                <button class="btn btn-primary" name="submit" type="submit">cari</button>
+            </div>
+        </div> 
+    </center>
+
+</form>                  
+</div>               
+
+</div>
 
 </div>
